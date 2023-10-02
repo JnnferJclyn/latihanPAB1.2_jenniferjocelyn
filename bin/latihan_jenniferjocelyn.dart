4f46ceec08a00dda45cd7d4ee2eb9219bc9d1548
VictoriaValensitaRobert <132757236+JnnferJclyn@users.noreply.github.com>
2023-10-02 15:02:09 +0700

    first commit

---
+++ bin/latihan_jenniferjocelyn.dart
@@ -0,0 +1,10 @@
+
+import 'PersegiPanjang.dart';
+
+void main() {
+  var persegiPanjang = PersegiPanjang(5, 3);
+
+  print('Luas Persegi panjang adalah ${persegiPanjang.hitungLuas()}');
+  print('Keliling Persegi panjang adalah ${persegiPanjang.hitungKeliling()}');
+  
+}
---
+++ test/latihan_jenniferjocelyn_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan_jenniferjocelyn/latihan_jenniferjocelyn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
